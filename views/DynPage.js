8051393baa08c0db66ae9cf93e20efef6c1b010f
853346@ACDM.DS.SAIT.CA <heinz.geipel@edu.sait.ca>
2020-06-11 17:14:07 -0600

    First Push for View files

---
+++ views/DynPage.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const greeting = require("../Module/greeting.js");
+
+exports.genConfirm = (result, lv_view, res)=>{
+
+// read file to get HTML response header 
+	fs.readFile(lv_view + "/confirmation.html", (err, htmlhead)=>{
+		if (err) throw err;
+// customize body using db response 
+		res.writeHead(200, { "Content-Type":"text/html" });
+		res.write(htmlhead);				
+// write data inside a table
+		res.write("<body>");
+		res.write("<h1>" + greeting.greet() + "</h1>");
+		res.write("<table border='1'>");
+		for (agent of result)
+		{
+			res.write("<tr>");
+			res.write("<td>" + agent.AgentId + "</td>"
+					+ "<td>" + agent.AgtLastName + "</td>"
+					+ "<td>" + agent.AgtFirstName + "</td>");
+			res.write("</tr>");
+		}
+		res.write("</table>");
+		res.write("</body>");
+		res.end();
+	});
+};
+	
+
