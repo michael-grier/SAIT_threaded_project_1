1d8ab9d039f766f2add1fac6ddc873628e997b53
853346@ACDM.DS.SAIT.CA <heinz.geipel@edu.sait.ca>
2020-06-11 17:06:21 -0600

    Second push

--- public/js/controler.js
+++ public/js/controler.js
@@ -1,20 +1,8 @@
-// Author: Robert Geipel
-// Date: 6/9/2020
-
-
 const dp = require("../View/DynPage.js");
 
-exports.updateDataSql = function update(req, conn)
+exports.updateDataSql = function update(result, conn, req)
 {
 // insert only if no such record		
-	var sql_sel = {
-		"sql": "SELECT * from agents where AgtLastName = ?",
-		"values": req.body.lname
-	};
-		conn.query(sql_sel, (err, result, fields)=>{
-		if (err = "NULL") {
-			console.log("Selection executed");
-		}
 		if (result.length > 0) {
 // data set not empty -> update agent
 			var sql_upd = { 
@@ -38,54 +26,86 @@ exports.updateDataSql = function update(req, conn)
 				if (err = "NULL") console.log("Insert executed");
 			});
 		};
-	});					    	
 }
 
 
-exports.updateDataMongo = function update(req, conn)
+exports.updateDataMongo = function update(result, dbo, req)
 {
 // insert only if no such record		
-    var dbo = conn.db("travelexperts");
-	var query = { AgtLastName: `${ req.body.lname }`};
-	dbo.collection("agents").find(query).toArray(function(err, result) {
-		if (err = "NULL") {
-			console.log("Select executed");
-			console.log(result.length);
-		}
  	if (result.length > 0) {
 // data set not empty -> update agent
-            console.log(result[0]._id);
-			var myquery = { _id: result[0]._id };
-			var myobj = { $set: { AgtFirstName: req.body.fname, AgtLastName: req.body.lname, AgtEmail: req.body.email } };
-			dbo.collection("agents").updateOne(myquery, myobj, function(err, res) {
-				if (err = "NULL") console.log("Update executed");    
-			});		
-		}
-		else
+        console.log(result[0]._id);
+		var myquery = { _id: result[0]._id };
+		var myobj = { $set: { AgtFirstName: req.body.fname, AgtLastName: req.body.lname, AgtEmail: req.body.email } };
+		dbo.collection("agents").updateOne(myquery, myobj, function(err, res) {
+			if (err = "NULL") console.log("Update executed");    
+		});		
+	}
+	else
 // data set empty -> insert new agent
-		{
-			var myobj = { AgtLastName: `${ req.body.lname }`, AgtFirstName: `${ req.body.fname }` };
-			dbo.collection("agents").insertOne(myobj, function(err, res) {
-				if (err = "NULL") console.log("Insert executed");    
-			});
-		};
-   });
+	{
+		var myobj = { AgtLastName: `${ req.body.lname }`, AgtFirstName: `${ req.body.fname }` };
+		dbo.collection("agents").insertOne(myobj, function(err, res) {
+			if (err = "NULL") console.log("Insert executed");    
+		});
+	};
+
 }
 
 
+exports.selDataSql = (parameters, callbackFunc)=>{
 
-exports.selData = (lname, conn, lv_view, res)=>{
-
-// run query to get updated record confirmed
+// run query to get record 
 	var sql = {
 		"sql": "SELECT * from agents where AgtLastName = ?",
-		"values": lname
+		"values": parameters[0]
 	};	
 
-// update view
-	conn.query(sql, (err, result, fields)=>{
+// update view or database depending on callback function
+	parameters[1].query(sql, (err, result, fields)=>{
 		if (err) throw err;
-		dp.genConfirm(result, lv_view, res);
+		if (callbackFunc == dp.genConfirm)
+		{
+			parameters[0] = result;
+			parameters[1] = parameters[2];
+			parameters[2] = parameters[3];
+			callbackFunc.apply(null, parameters);
+		}
+		else 
+		{
+			parameters[0] = result;
+			console.log(result);
+			callbackFunc.apply(null, parameters);
+		};
 	});
-	
+		
 }
+
+exports.selDataMongo = (parameters, callbackFunc)=>{
+
+// run query to get record
+	var dbo = parameters[1].db("travelexperts");
+	var query = { AgtLastName: `${ parameters[0] }`};
+
+// update view or database depending on callback function
+	dbo.collection("agents").find(query).toArray(function(err, result) {
+		if (err) throw err;
+
+		if (callbackFunc == dp.genConfirm)
+		{
+			parameters[0] = result;
+			parameters[1] = parameters[2];
+			parameters[2] = parameters[3];
+			callbackFunc.apply(null, parameters);
+		}
+		else 
+		{
+			parameters[0] = result;
+			parameters[1] = dbo
+			console.log("selected data: " + result);
+			callbackFunc.apply(null, parameters);
+		};
+	});
+		
+}
+
