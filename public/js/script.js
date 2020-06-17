3574192945631060786a563088fb50bf13037649
Kevin Duong <kd.kevinduong@gmail.com>
2020-06-17 16:31:32 -0600

    reminders don't repeat twice

--- public/js/script.js
+++ public/js/script.js
@@ -5,19 +5,27 @@ var reminders = ["Remember, cell phone use is not permitted during an exam",
 				"Stay hydrated by grabbing a glass of water",
 				"Make sure you're comfortable while taking your exam"];
 
-//Variable for the reminder interval	
+//Variable for the reminder interval
 var remindInter;
 
 //Index number used to get a reminder, randomly generated initially
 var i = Math.floor(Math.random() * reminders.length);
 
 //Starts the interval
-function addReminder(){
+function timeReminder(){
 	remindInter = setInterval(switchReminder, 5000);
 }
 
 //Switch reminder to a random one
 function switchReminder(){
-	i = Math.floor(Math.random() * reminders.length);
+	var newIndex = Math.floor(Math.random() * reminders.length);
+
+	//Check if new random number is the same as the previous
+	//increment newIndex if it's the same
+	if (i == newIndex){
+		newIndex = (i + 1) % reminders.length;
+	}
+	
+	i = newIndex;
 	document.getElementsByClassName("reminder")[0].innerHTML = reminders[i];
 }
